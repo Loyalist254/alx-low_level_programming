a8acf8bbf2f1fc534065bdabb25faa825a56d19a
Loyal <loyal@gmail.com>
2023-03-27 12:55:23 -0700

    program that generates random valid passwords for the program 101-crackme.

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -10,10 +10,9 @@ int main(void)
 {
 	int nb;
 
-
 	nb = _atoi("98");
 	printf("%d\n", nb);
-	nb = _atoi("-402");
+	nb = _atoi("-402");   
 	printf("%d\n", nb);
 	nb = _atoi("          ------++++++-----+++++--98");
 	printf("%d\n", nb);
@@ -27,5 +26,5 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", nb);
-	return (0);
+    	return (0);
 }
